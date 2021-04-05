49703d695abb7214e94bb7bc0dfa56dbe71c8b5b
donggun <tnpdlzi@naver.com>
2021-04-05 15:46:05 +0900

    fix: url config 함수 수정

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 
 from bookmark.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('bookmark/', BookmarkLV.as_view(), name='index'),
-    path('bookmark/(?P<pk>\d+)/$'), BookmarkDV.as_view(), name='detail'),
+    url(r'^bookmark/', BookmarkLV.as_view(), name='index'),
+    url(r'^bookmark/(?P<pk>\d+)/$'), BookmarkDV.as_view(), name='detail'),
 ]
